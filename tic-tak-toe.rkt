c69a13051f342dd245bbd12dad11e482d92a0b67
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2017-10-18 10:05:25 +0900

    update comments

--- tic-tak-toe.rkt
+++ tic-tak-toe.rkt
@@ -1,5 +1,12 @@
 #lang racket
 ;;-*- mode: scheme -*-
+;;
+;; コメント熟読せよ。
+;;
+;; * GUI からプログラムしてみる
+;; * 判定ルーチンを独立させる
+;;
+
 (require racket/gui/base)
 
 ;; ゲームの縦横
@@ -15,6 +22,8 @@
       (else (nth (rest obj) (- n 1))))))
 
 ;; (map-index '(a b c d)) は ((0 a) (1 b) (2 c) (3 d)) を返す。
+;; 味方の石が置かれた位置を知る時に使う。
+;; a c に味方の石があるとき (0 2) が返るような関数をどこかに定義する。
 (define map-index
   (λ (lst)
     (map list (range (length lst)) lst)))
@@ -36,6 +45,7 @@
     (not (equal? (send btn get-label) ""))))
 
 ;; ボタン btn の背景を mark とする。
+;; すでにマークされていたらエラー。
 (define mark
   (λ (btn mark)
     (if (marked? btn)
@@ -46,31 +56,33 @@
           (send btn set-label mark)
           #t))))
 
-;; リスト buttons 中のマーク m がついたボタンのインデックスを返す。
-;; FIXME, 毎回 map-index を呼ぶのは非効率だろ？
+;; リスト buttons-index 中のマーク m がついたボタンのインデックスを返す。
+;; car を忘れずに。
 (define select-index
   (λ (m)
     (map car
          (filter (λ (ib) (equal? m (send (second ib) get-label)))
-                 (map-index buttons)))))
+                 buttons-index))))
 
 ;; FIXME, インデックスから縦横斜めに揃っているかどうかを判定。
-;; 揃っていたら #t, そうでなければ #f を返す。宮崎に任すと喜ぶか？
-;; インチキして、長さで判定。
+;; 揃っていたら #t, そうでなければ #f を返す。
+;; インチキして長さで判定。これでは正しく判定できないよ。
+;; 宮崎の出番だ。
 (define wins?
   (lambda (marks)
     (> (length marks) 3)))
 
 ;; 勝敗判定
+;; select-index で味方の石のある場所のリスト、
+;; wins? で石が並んでるかどうかを判定、メッセージをだす。
+;; 勝敗がつかない時は #f で抜ける。
+;; FIXME, 勝敗ついた時はゲームを止めなくちゃな。
 (define judge
   (lambda ()
-    (let ((os (select-index "o"))
-          (xs (select-index "x")))
-      (cond
-       ((wins? os) (message "o wins"))
-       ((wins? xs) (message "x wins"))
-       (else #f)))))
-
+    (cond
+     ((wins? (select-index "o")) (message "o wins"))
+     ((wins? (select-index "x")) (message "x wins"))
+     (else #f))))
 
 ;; 相手に手を渡す。
 ;; 空いている目を調べ、ランダムにその一つにマーク "x" を入れる。
@@ -89,13 +101,19 @@
 ;; そうでなければ "o" を表示し、相手に手を渡す関数を呼ぶ。
 ;; 2重ループが list の list を返すので、
 ;; map 関数が楽になるよう flatten で平たくしておく。
+;; panel の代わりに軽い pane を使う。あんまり変わらん。
 (define buttons
-  (flatten (for/list ([row (range rows)])
-                     (let ((pane (new horizontal-pane% [parent frame])))
-                       (for/list ([col (range cols)])
-                                 (new button% [parent pane]
-                                      [label ""]
-                                      [callback (λ (b e)
-                                                  (when (mark b "o")
-                                                    (judge)
-                                                    (opposite)))]))))))
+  (flatten
+   (for/list ([row (range rows)])
+             (let ((pane (new horizontal-pane% [parent frame])))
+               (for/list ([col (range cols)])
+                         (new button% [parent pane]
+                              [label ""]
+                              [callback (λ (b e)
+                                          (when (mark b "o")
+                                            (judge)
+                                            (opposite)))]))))))
+
+;; ボタンのリストにインデックスをつけたリストをあらかじめ作っておく。
+;; (b0 b1 b2) => ((0 b0) (1 b1) (2 b2)) みたいに。
+(define buttons-index (map-index buttons))
